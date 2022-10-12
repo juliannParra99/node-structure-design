94a10bf97e15f0c11b6b58efd6868d915239e55f
julianParrax <julianfranco.parra@gmail.com>
2022-10-12 13:22:05 -0300

    Writing middleware

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 3000
-const birds = require('./router/bird')
-const paramsNext = require('./router/paramsNext')
-const methods  = require('./router/httpMethods')
-
-app.use('/static',express.static( __dirname + '/public'));
-
-//manejador de errores
-
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-//traigo los archivos  de la carpeta routes
-//voy a usar en esa ruta lo que traigo de el archivo bird contenido en la constante birds
-app.use('/bird', birds)
-app.use('/paramsNext',paramsNext)
-app.use('/methods',methods)
 
 
-  
-  //manejador de respuestas  404; va al final del resto de contenido
-app.use(function(req, res, next) {
-  res.status(404).send('Sorry cant find that!');
-}); 
+//middlewew: cuando se pide una solicitud en el navegador (request), se guarda la hora de el momento en que se relizo la solicitud
+const requestTime = function (req, res, next) {
+  req.requestTime = Date.now()
+  console.log(req.requestTime)
+  next()
+}
+ //aca se ejecuta el middlewere
+app.use(requestTime)
 
-app.listen(port, () => {
-  console.log(`server on port: ${port}`)
+app.get('/', (req, res) => {
+  let responseText = 'Hello World!<br>'
+  responseText += `<small>Requested at: ${req.requestTime}</small>`
+  res.send(responseText)
 })
+
+app.listen(3000, ()=>
+console.log(`server on port ${3000}`)
+)
